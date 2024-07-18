71915b610913b8d88a6ea2bd3c1d190303ebb5aa
Simon Guigui <simon.guigui@parkoview.com>
2024-07-18 15:46:58 +0200

    fix: building debian packages with single binary application fails

--- src/wheel2deb/utils.py
+++ src/wheel2deb/utils.py
@@ -1,10 +1,13 @@
+import os
 import subprocess
 from pathlib import Path
 from typing import List, Tuple
 
 
 def shell(args: List[str], cwd: Path | None = None) -> Tuple[str, int]:
+    env = os.environ.copy()
+    env.pop("LD_LIBRARY_PATH", None)
     result = subprocess.run(
-        args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=env
     )
     return result.stdout.decode("utf-8"), result.returncode
